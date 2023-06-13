e181f005ed25fcc16fad720ac90d4132ff048387
Daniel Quinteros <quinterosm.daniel@gmail.com>
2023-06-12 21:46:47 -0400

    feat: featch api

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,23 @@
+import { useRouteError } from "react-router-dom"
+import Menu from "./Menu"
+
 const Error = () => {
+
+    const routeError = useRouteError();
+
   return (
-    <h1>Error</h1>
+    <>
+        <Menu />
+        <div className="px-4 py-5 my-5 text-center">
+            <h1 className="display-5 fw-bold text-danger">Error</h1>
+            <div className="col-lg-6 mx-auto">
+            <p className="lead mb-4">Esta página no existe, te recomendamos volver al inicio.</p>
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <button type="button" className="btn btn-outline-danger disabled btn-lg px-4">{routeError.error.message}</button>
+            </div>
+            </div>
+        </div>
+    </>
   )
 }
 
--- src/components/Usuarios.jsx
+++ src/components/Usuarios.jsx
@@ -1,12 +1,60 @@
+import { useEffect, useState } from "react"
 import Menu from "./Menu"
+import '../usuarios.css'
 
 const Usuarios = () => {
-  return (
-    <>
-        <Menu />
-        <h1>Usuarios</h1>
-    </>
-  )
+    // Los usuarios estan listo para cargarse dejo el arreglo vacio
+    const [users, setUsers] = useState([])
+
+
+    // Preparamos todo para hervir agua
+    const getUsers = async () => {
+        // Llama a la función
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        
+        // Tranforma lo que me llegue a un formato que pueda leer -> JSON -> FORMATO LEGIBLE POR JS
+        const usersApi = await response.json();
+
+        // Lo que me responda la api lo voy a setear
+        setUsers(usersApi);
+    }
+
+    useEffect(() => {
+        // Ejecutando la función
+        // Estamos prendiendo el botón del
+        getUsers()
+    }, [])
+
+    
+    const renderUsers = () => {
+        if(users.length > 0){
+            const allUser = users.map(user => {
+                return(
+                    <div className="card wrapper-card mt-1 mb-1" key={user.id}>
+                        <div className="card-body">
+                            <h5 className="card-title">{user.name}</h5>
+                            <h6 className="card-subtitle mb-2 text-body-secondary">{user.username}</h6>
+                            <p className="card-text">{user.email}</p>
+                            <a href="#" className="card-link">{user.phone}</a>
+                        </div>
+                    </div>
+                )
+            })
+            return allUser;
+        } else {
+            return <h1>No existen Usuarios</h1>
+        }
+    }
+
+    //Render
+    return (
+        <>
+            <Menu />
+            <div className="container wrapper-container">
+                {renderUsers()}
+            </div>
+        </>
+    )
 }
 
 export default Usuarios
